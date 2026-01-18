b8cb25a73a01cd8938c0265edeb54ddaea58d9b5
asa129 <asa@takeshitaasakanoMacBook-Air.local>
2026-01-18 23:44:39 +0900

    コメント追加

--- number_game.py
+++ number_game.py
@@ -1,13 +1,16 @@
 import random
 
+# Function to input minimum and maximum numbers
 def input_numbers():
     min = input('Please input minimum number: ')
     max = input('Please input maximum number: ')
     return min, max
 
+# Function to input user's prediction
 def input_prediction():
     return input('Guess the random number that will be output: ')
 
+# Function to judge if the prediction is correct
 def judge(min, max, prediction):
     answer = random.randint(min, max)
     return prediction == answer
